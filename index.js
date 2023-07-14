df4803efb816c101655dd04dcdac6d91efc8844e
HopefulBee <127155130+HopefulBee@users.noreply.github.com>
2023-07-14 12:58:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,43 +22,36 @@ document.addEventListener("keydown", function(event){
 function makeSound(key) {
     switch (key) {
         case "w":
-                this.style.color = "blue";
                 var tom1 = new Audio("sounds/tom-1.mp3");
                 tom1.play();
             break;
         
         case "a":
-                this.style.color = "blue";
                 var tom2 = new Audio("sounds/tom-2.mp3");
                 tom2.play();
             break;
 
         case "s":
-                this.style.color = "blue";
                 var tom3 = new Audio("sounds/tom-3.mp3");
                 tom3.play();
             break;
 
         case "d":
-                this.style.color = "blue";
                 var tom4 = new Audio("sounds/tom-4.mp3");
                 tom4.play();
             break;
 
         case "j":
-                this.style.color = "blue";
                 var crash = new Audio("sounds/crash.mp3");
                 crash.play();
             break;
 
         case "k":
-                this.style.color = "blue";
                 var snare = new Audio("sounds/snare.mp3");
                 snare.play();
             break;
 
         case "l":
-                this.style.color = "blue";
                 var kick = new Audio("sounds/kick-bass.mp3");
                 kick.play();
             break;
